12a25dd1c6c1d85ff25a2b6ac3bf10df632c2a31
Davis <daviszhou@users.noreply.github.com>
2019-03-13 17:05:58 -0400

    Script for removal of blood vessels based on local thresholding

---
+++ threshvesselremoval.m
@@ -0,0 +1,33 @@
+function image_output = threshvesselremoval(image_input, vessels_image_input)
+
+debugging = false;
+
+vessels_image_input = im2double(vessels_image_input);
+vessels_image_input = squeeze(vessels_image_input(:,:,1));
+
+vessel_mask = bradley(vessels_image_input);
+vessel_mask = ~vessel_mask;
+
+if debugging
+    figure(); imagesc(vessel_mask); colormap gray; axis image; axis off; title('1');
+    vessel_mask = bwareaopen(vessel_mask, 10);
+    figure(); imagesc(vessel_mask); colormap gray; axis image; axis off; title('1');
+    vessel_mask = bwareaopen(vessel_mask, 14);
+    figure(); imagesc(vessel_mask); colormap gray; axis image; axis off; title('1');
+    vessel_mask = bwareaopen(vessel_mask, 16);
+    figure(); imagesc(vessel_mask); colormap gray; axis image; axis off; title('1');
+else
+    vessel_mask = bwareaopen(vessel_mask, 16);
+end
+
+se = strel('disk', 4);
+vessel_mask = imdilate(vessel_mask, se);
+
+if debugging
+    figure(); imagesc(vessel_mask); colormap gray; axis image; axis off; title('Initial Vessels Mask');
+end
+
+image_output = immultiply(~vessel_mask, image_input);
+
+end
+
