5a2c9c28682b015aeb29dca286143e2ddf786efd
Davis <daviszhou@users.noreply.github.com>
2019-04-03 17:00:59 -0400

    Remove deprecated comments and debug output

--- squarecrop.m
+++ squarecrop.m
@@ -2,15 +2,11 @@ function [output_image] = squarecrop(image, side_length)
     % Crop image into square given length of sides
     % Crops the boarders while centering the image
     % [matrix] = squarecrop(matrix, double)
-    %
-    % version davis 2019.01.25
-    % TODO allow specification of center
     
     if ~isnumeric(side_length)
         error('Crop Error: crop side length must be input as a number');
     end
     
-    disp(side_length)
     if (size(image,1) > side_length || size(image,2) > side_length)
         start_x = round((size(image,1) - side_length) / 2) + 1;
         start_y = round((size(image,2) - side_length) / 2) + 1;
