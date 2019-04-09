3a5941fa146fdbb6dfe8b71f16db239c7de83668
Davis <daviszhou@users.noreply.github.com>
2019-04-09 17:18:16 -0400

    Add input options to background correction script

--- backgroundcorrection.m
+++ backgroundcorrection.m
@@ -1,7 +1,7 @@
-function [im_adjusted] = backgroundcorrection(im_roi, im_background, center, radius)
+function im_adjusted = backgroundcorrection(im_roi, im_background, center, radius, has_adjustment_constant, has_disk_mask)
 % Background Correction corrects  uneven illumination in an retinal disk image
 %   Correction is based on slice of the retina that does not contain pathology
-%   By default, the choroid layer is used for im_background
+%   By default, the choroid layer is used for the background
 %   Optic disk is excluded from analysis and set as NaN
 
 figure(); imagesc(im_roi); colormap('gray'); axis('square'); title('RPC');
@@ -11,30 +11,40 @@ figure(); imagesc(im_background); colormap('gray'); axis('square'); title('Choro
 im_roi_lp = imgaussfilt(im_roi, 5);
 im_background_lp = imgaussfilt(im_background, 5);
 
-% Exclude disk from analysis
-disk_mask = createCirclesMask(im_background, center/6, radius/6);
+if has_adjustment_constant
+    
+    if has_disk_mask
+    % Exclude disk from analysis
+    disk_mask = createCirclesMask(im_background, center/6, radius/6);
 
-% im_roi = imgaussfilt(im_roi, 2);
-% im_background = imgaussfilt(im_background, 2);
-im_roi = immultiply(im_roi, ~disk_mask);
-im_background = immultiply(im_background, ~disk_mask);
+    % im_roi = imgaussfilt(im_roi, 2);
+    % im_background = imgaussfilt(im_background, 2);
+    im_roi = immultiply(im_roi, ~disk_mask);
+    im_background = immultiply(im_background, ~disk_mask);
 
-im_roi_lp = immultiply(im_roi_lp, ~disk_mask);
-im_background_lp = immultiply(im_background_lp, ~disk_mask);
+    im_roi_lp = immultiply(im_roi_lp, ~disk_mask);
+    im_background_lp = immultiply(im_background_lp, ~disk_mask);
+    end
+    
+    % Calculate adjustment constant
+    im_roi_lp(isnan(im_roi_lp)) = 0;
+    top = nanmean(im_roi_lp(:));
+    im_background_lp(isnan(im_background_lp)) = 0;
 
-% Calculate adjustment constant
-im_roi_lp(isnan(im_roi_lp)) = 0;
-top = nanmean(im_roi_lp(:));
-im_background_lp(isnan(im_background_lp)) = 0;
+    fraction = im_roi ./ im_background;
 
-fraction = im_roi ./ im_background;
+    % fraction(isnan(fraction)) = 0;
+    bottom = nanmean(fraction(:));
+    adjustment_constant = top / bottom;
+    disp('Adjustment Constant is' + adjustment_constant)
 
-% fraction(isnan(fraction)) = 0;
-bottom = nanmean(fraction(:));
-adjustment_constant = top / bottom
+    % Apply choroidal correction
+    im_adjusted = im_roi ./ im_background .* adjustment_constant;
+    
+else
+    im_adjusted = im_roi ./ im_background .* 150;
+end
 
-% Apply choroidal correction
-%im_adjusted = im_roi ./ im_background .* 150;
-im_adjusted = im_roi ./ im_background .* 150;
 figure(); imagesc(im_adjusted); colormap('gray'); axis('square'); title('Adjusted'); caxis([0 255])
+
 end
