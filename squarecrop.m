f50d50ecbdead93b43e53218ed4ed9cfdb0ba644
Davis <daviszhou@users.noreply.github.com>
2019-07-24 13:43:03 -0400

    Update variables for clarity

--- squarecrop.m
+++ squarecrop.m
@@ -1,33 +1,31 @@
-function [output_image] = squarecrop(image, side_length)
+function [output_image] = squarecrop(image, output_side_length)
     % Crop image into square given length of sides
     % Crops the boarders while centering the image
     % [matrix] = squarecrop(matrix, double)
     
-    if ~isnumeric(side_length)
+    if ~isnumeric(output_side_length)
         error('Crop Error: crop side length must be input as a number');
     end
     
-    if (size(image,1) > side_length || size(image,2) > side_length)
-        start_x = round((size(image,1)/2 - side_length) / 2) + 1;
-        start_y = round((size(image,2)/2 - side_length) / 2) + 1;
-        crop_length = side_length - 1;
+    [image_length_x, image_length_y] = size(image);
+    
+    if (image_length_x > output_side_length || image_length_y > output_side_length)
+        start_x = round((image_length_x/2 - output_side_length) / 2) + 1;
+        start_y = round((image_length_y/2 - output_side_length) / 2) + 1;
+        crop_length = output_side_length - 1;
         output_image = image(start_x:start_x+crop_length, start_y:start_y+crop_length);
         % disp(size(output_image)); %Debugging
-    elseif (size(image,1) == side_length && size(image,2) == side_length)
+    elseif (image_length_x == output_side_length && image_length_y == output_side_length)
         disp('Warning: input image has same demensions as crop size. No changes were made.')
         output_image = image;
-    elseif (size(image,1) < side_length || size(image,2) < side_length)
+    elseif (image_length_x < output_side_length || image_length_y < output_side_length)
         disp('Warning: input image is smaller than crop size. Image will be placed in a canvas with crop-size dimensions')
         
-        [image_length_x image_length_y] = size(image);
-
-        larger_canvas=uint8(zeros(side_length, side_length));
-        startpix_x=side_length/2 - size(image,1)/2 + 1; % set x displacement
-        startpix_y=side_length/2 - size(image,2)/2 + 1; % set y displacement
-        larger_canvas_temp = larger_canvas;
-
-        larger_canvas_temp(startpix_x:startpix_x+image_length_x-1, startpix_y:startpix_y+image_length_y-1)=image;
-        output_image=larger_canvas_temp;
+        larger_canvas=uint8(zeros(output_side_length, output_side_length));
+        startpix_x=output_side_length/2 - image_length_x/2 + 1; % set x displacement
+        startpix_y=output_side_length/2 - image_length_y/2 + 1; % set y displacement
+        larger_canvas(startpix_x:startpix_x+image_length_x-1, startpix_y:startpix_y+image_length_y-1)=image;
+        output_image=larger_canvas;
     else
         error('Crop Error: check squarecrop input image')
     end
